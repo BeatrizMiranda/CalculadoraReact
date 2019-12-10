18869851a53009bcb004d484fbaaddd24cd8d4c7
Beatriz Miranda <beatriz.miranda@acct.global>
2019-12-10 17:00:01 -0300

    fixing bug

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -31,16 +31,15 @@ export default class Calculator extends Component {
         if(this.state.current === 0){
             this.setState({operation, current:1, clearDisplay: true})
         }else{
-            const equals = operation === '='
-            const currentOperation = this.state.operation
-
-            const values = [...this.state.values]
+            const equals = operation === '=' //true se operation for =
+            const currentOperation = [this.state.operation]
+            const values = {...this.state.values}
             try{
                 values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
             }catch(e){
                 values[0] = this.state.values[0]
             }
-            
+
             values[1] = 0
 
             this.setState({
@@ -49,7 +48,6 @@ export default class Calculator extends Component {
                current: equals ? 0 : 1,
                clearDisplay: !equals,
                values
-
             })
         }
     }
